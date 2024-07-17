74d479518d7d5967d8714909be35f313238de653
Ekkaira <denysxchebotar@gmail.com>
2024-07-17 19:39:24 +0300

    demo rive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Alignment,
+  Fit,
+  Layout,
+  useRive,
+  useStateMachineInput,
+} from '@rive-app/react-canvas';
+import { useState, useEffect } from 'react';
+import './index.css';
 
-function App() {
+export const RiveDemo = () => {
+  const STATE_MACHINE_NAME = 'State Machine 1';
+  const BOOST_INPUT = 'boost';
+  const TAP_INPUT = 'tap';
+
+  const { RiveComponent, rive } = useRive({
+    src: 'potato.riv',
+    stateMachines: STATE_MACHINE_NAME,
+    layout: new Layout({
+      fit: Fit.FitWidth,
+      alignment: Alignment.Center,
+    }),
+    autoplay: true,
+  });
+
+  const onTapInput = useStateMachineInput(rive, STATE_MACHINE_NAME, TAP_INPUT);
+  const boostToggle = useStateMachineInput(rive, STATE_MACHINE_NAME, BOOST_INPUT);
+
+  const [boostActive, setBoostActive] = useState(false);
+
+  useEffect(() => {
+    if (rive) {
+      if (boostActive) {
+        setBoostActive(true);
+        boostToggle.value = true;
+      } else {
+        setBoostActive(false);
+        boostToggle.value = false;
+      }
+    }
+  }, [boostActive, boostToggle]);
+
+  if (rive) {
+    console.log(rive.contents);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <RiveComponent />
+      <div className="footer">
+        <button onClick={() => setBoostActive(!boostActive)}>
+          {boostActive ? 'Deactivate Boost' : 'Activate Boost'}
+        </button>
+        <button onClick={() => onTapInput.fire()}>Tap</button>
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="RiveContainer">
+      <RiveDemo />
     </div>
   );
 }
-
-export default App;
